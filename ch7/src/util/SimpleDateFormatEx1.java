f57874ed2606cbe71e1d9d1a5587d67f87f73870
i0231588 <128015233+i0231588@users.noreply.github.com>
2023-03-16 17:41:34 +0900

    Update SimpleDateFormatEx1.java

--- ch7/src/util/SimpleDateFormatEx1.java
+++ ch7/src/util/SimpleDateFormatEx1.java
@@ -4,7 +4,7 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 public class SimpleDateFormatEx1{
 public static void main(String[] args) {
-		//시스템 날짜와 시간 가져오기
+		
 		Date date = new Date();
 		System.out.println(date);
 		
