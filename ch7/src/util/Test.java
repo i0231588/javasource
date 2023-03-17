323fff9834ba18f2c67f23b5e1c667ea4c665e20
young jun <i0231588@hanmail.net>
2023-03-17 10:44:22 +0900

    인스턴스

--- ch7/src/util/Test.java
+++ ch7/src/util/Test.java
@@ -1,26 +1,13 @@
 package util;
 
 public class Test {
-	
 	private String name;
 	private int no;
 	private String tel;
 	
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-}
+	public void print() {}
+	public int sum() {
+		return 0;
+		}
+	}
